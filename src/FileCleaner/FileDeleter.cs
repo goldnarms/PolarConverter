a3d845b979077ba1ebc4bc0a9bdc97c055679055
Arnstein Johansen <arnstej@gmail.com>
2014-02-11 18:16:25 +0100

    Added webjob

---
+++ src/FileCleaner/FileDeleter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace FileCleaner
+{
+    public class FileDeleter
+    {
+        public static void DeleteOldFiles()
+        {
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("polarfiles");
+            container.FetchAttributes();
+            foreach (var blob in from item in container.ListBlobs() where item.GetType() == typeof(CloudBlockBlob) select (CloudBlockBlob)item into blob let dateUploaded = Convert.ToDateTime(blob.Properties.LastModifiedUtc) where dateUploaded < DateTime.Now.AddDays(-5) select blob)
+            {
+                blob.Delete();
+            }
+        }
+
+    }
+}
--- src/FileCleaner/Program.cs
+++ src/FileCleaner/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Linq;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Jobs;
-using Microsoft.WindowsAzure.StorageClient;
+﻿using Microsoft.WindowsAzure.Jobs;
 
 namespace FileCleaner
 {
@@ -10,21 +6,8 @@ namespace FileCleaner
     {
         static void Main(string[] args)
         {
-            var job = new JobHost();
-            job.RunAndBlock();
-            DeleteOldFiles();
-        }
-
-        public static void DeleteOldFiles()
-        {
-            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
-            var blobClient = storageAccount.CreateCloudBlobClient();
-            var container = blobClient.GetContainerReference("polarfiles");
-            container.FetchAttributes();
-            foreach (var blob in from item in container.ListBlobs() where item.GetType() == typeof(CloudBlockBlob) select (CloudBlockBlob)item into blob let dateUploaded = Convert.ToDateTime(blob.Properties.LastModifiedUtc) where dateUploaded < DateTime.Now.AddDays(-5) select blob)
-            {
-                blob.Delete();
-            } 
+            var host = new JobHost();
+            host.Call(typeof(FileDeleter).GetMethod("DeleteOldFiles"));
         }
     }
 }
