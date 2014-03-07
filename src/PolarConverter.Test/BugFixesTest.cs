df366846056a2b3291d15a549a22b6c36bcecf96
Arnstein Johansen <arnstej@gmail.com>
2014-03-07 20:56:08 +0100

    test

--- src/PolarConverter.Test/BugFixesTest.cs
+++ src/PolarConverter.Test/BugFixesTest.cs
@@ -22,7 +22,7 @@ namespace PolarConverter.Test
             var polarData = new PolarData
             {
                 HrmData = hrmData,
-                UserInfo = new UserInfo() { TimeZoneOffset = 1 },
+                UserInfo = new UserInfo { TimeZoneOffset = 1 },
                 Modus = modus,
                 ModusVerdi = modusVerdi,
                 HarCadence = modus == "SMode" ? (modusVerdi.Substring(1, 1) == "1") : modusVerdi.Substring(0, 1) == "0",
