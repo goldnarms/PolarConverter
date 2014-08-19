c0929eec75d814b8bd419e67b727cac52900b17a
Arnstein Johansen <arnstej@gmail.com>
2014-08-19 22:29:32 +0200

    Stupid js files

--- src/PolarConverter.JSWeb/App/app.js
+++ src/PolarConverter.JSWeb/App/app.js
@@ -15,4 +15,4 @@ var PolarConverter;
             cfpLoadingBarProvider.includeSpinner = false;
         }]).controller("uploadCtrl", PolarConverter.UploadController.prototype.injection()).controller("fileDestroyCtrl", PolarConverter.FileDestroyController.prototype.injection()).controller("userfilesCtrl", PolarConverter.UserFilesController.prototype.injection()).controller("userProfileCtrl", PolarConverter.UserProfile.prototype.injection()).service("facebookShareService", PolarConverter.FacebookShareService.prototype.injection()).service("fileService", PolarConverter.FileService.prototype.injection()).service("userService", PolarConverter.UserService.prototype.injection()).service("common", PolarConverter.CommonService.prototype.injection()).directive("shareOnGooglePlus", PolarConverter.ShareOnGooglePlus.prototype.injection()).directive("shareOnTwitter", PolarConverter.ShareOnTwitter.prototype.injection()).directive("shareOnFacebook", PolarConverter.ShareOnFacebook.prototype.injection());
 })(PolarConverter || (PolarConverter = {}));
-//# sourceMappingURL=app.js.map
+//# sourceMappingURL=App.js.map
