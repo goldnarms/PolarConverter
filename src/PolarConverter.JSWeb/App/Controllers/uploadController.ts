4a4603e946d32e71eb79758b2421da60becbf2ff
Arnstein Johansen <arnstej@gmail.com>
2014-10-07 22:28:47 +0200

    Small cleanup

--- src/PolarConverter.JSWeb/App/Controllers/uploadController.ts
+++ src/PolarConverter.JSWeb/App/Controllers/uploadController.ts
@@ -81,7 +81,7 @@ module PolarConverter {
             this.tweetText = "I have just converted my Polar files to Endomondo compatible files using #polarconverter at ";
             this.isMetricWeight = true;
             this.isConverting = false;
-            this.uploadViewModel = <PolarConverter.UploadViewModel>{ polarFiles: [], forceGarmin: false, gender: "m" };
+            this.uploadViewModel = <PolarConverter.UploadViewModel>{ polarFiles: [], forceGarmin: false, gender: "m"};
             var url = "/api/upload";
             this.options = {
                 autoUpload: true,
