72385936fe492491fa85a6208218d7c5af0d8ec7
Arnstein Johansen <arnstej@gmail.com>
2014-11-04 22:00:59 +0100

    Added Pro Controller

---
+++ src/PolarConverter.JSWeb/Controllers/ProController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PolarConverter.JSWeb.Controllers
+{
+    public class ProController : Controller
+    {
+        // GET: Pro
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult OrderPro()
+        {
+            return View("Index");
+        }
+    }
+}
